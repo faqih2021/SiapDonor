0b04cf5addd3e6923361b049332f62046fd465b3
unknown <klopster070@gmail.com>
2024-06-14 00:15:34 +0700

    loginauth-iqbal

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -29,7 +29,49 @@
                 <a href="/" class="nav-item nav-link active">Home</a>
                 <a href="/information" class="nav-item nav-link">Information</a>
                 <a href="/donation" class="nav-item nav-link">Blood Donation</a>
-                <a href="/" class="nav-item nav-link">Login</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('admin.dashboard') }}">
+                                        {{ __('Dashboard') }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
                 {{-- <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                     <div class="dropdown-menu border-light m-0">
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/information', function () {
     return view('information');
@@ -13,3 +18,26 @@ Route::get('/information', function () {
 Route::get('/donation', function () {
     return view('donation');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+
+Route::get('/table', function () {
+    return view('admin.table');
+});
+
+// Route::get('/dashboard', function () {
+//     return view('admin.dashboard');
+// });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
